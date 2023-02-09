68fd0ce401f1e6b5d83f16dd92a3a90a679a2458
alex <alexmuthini3@gmail.com>
2023-02-09 15:29:42 +0300

    modified __init__method of base_model

--- models/base_model.py
+++ models/base_model.py
@@ -28,6 +28,8 @@ class BaseModel:
                         self.__dict__[key] = datetime.fromisoformat(value)
                     else:
                         self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """Returns the string representation of this model class."""
