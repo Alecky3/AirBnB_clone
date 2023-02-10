e6729f03ecf0797175ce26f60c573772002d9e82
alex <alexmuthini3@gmail.com>
2023-02-10 12:53:47 +0300

    modified file_storage

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-""" Creates a unique FileStorage instance for the application."""
 
-from file_storage import FileStorage
+from models.engine.file_storage  import FileStorage
 storage = FileStorage()
 storage.reload()
